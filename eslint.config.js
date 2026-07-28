f82a595f9a95981dbfb9dbe402c3450f8a348c04
Meet Chauhan <meetchauhan@alitersolutions.com>
2026-07-28 16:08:51 +0530

    refactor(workspace): align file structure with ESLint v9 flat config and modular infra directory

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  {
+    plugins: {
+      '@nx': nx,
+    },
+    rules: {
+      '@nx/enforce-module-boundaries': [
+        'error',
+        {
+          enforceBuildableLibDependency: true,
+          allow: [],
+          depConstraints: [
+            {
+              sourceTag: 'scope:feature',
+              onlyDependOnLibsWithTags: [
+                'scope:data-access',
+                'scope:domain',
+                'scope:util',
+                'scope:ui',
+              ],
+            },
+            {
+              sourceTag: 'scope:data-access',
+              onlyDependOnLibsWithTags: ['scope:domain', 'scope:util'],
+            },
+            {
+              sourceTag: 'scope:domain',
+              onlyDependOnLibsWithTags: ['scope:util'],
+            },
+            {
+              sourceTag: 'scope:ui',
+              onlyDependOnLibsWithTags: ['scope:util', 'scope:ui'],
+            },
+            {
+              sourceTag: 'scope:util',
+              onlyDependOnLibsWithTags: ['scope:util'],
+            },
+          ],
+        },
+      ],
+    },
+  },
+];
